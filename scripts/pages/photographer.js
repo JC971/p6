0149286517db96bc60c327757014ccb06b299f62
jean-claude <jean-claude.grech@orange.fr>
2023-06-09 11:31:39 +0200

    media

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,3 +1,4 @@
+/*
 function photographerFactory(data) {
   const { name, portrait, city, country, tagline, price, id } = data;
 
@@ -12,7 +13,64 @@ function photographerFactory(data) {
   return { name, picture, city, country, price, id, getUserCardDOM };
 
 };
+*/
 
+
+
+
+const url = window.location.href;
+console.log('url de la page est', url);
+let photographId = url.split("=")[1];
+
+console.log(photographId)
+fetch('data/photographers.json')
+.then(response => response.json())
+.then(data => {
+  const media = data['media'];
+  const portfolio = [];
+  const photographer = data['photographers'];
+  let photographCourant ;
+// on récupère les photos du photographe
+  for (let i = 0; i < media.length; i++) {
+    if (media[i].photographerId==photographId)
+    {
+      portfolio.push(media[i])
+
+
+     };
+
+
+
+
+
+
+
+
+/*
+
+    // Créer un élément <p> pour afficher le nom du photographe
+    const nameElement = document.createElement('p');
+    nameElement.innerHTML = photographerData.name;
+
+    // Ajouter l'élément au conteneur approprié dans votre page HTML
+    const container = document.getElementById('photographersContainer');
+  
+    container.appendChild(nameElement);*/
+  }
+
+// on cherche les informations du photographe
+  for (let i = 0; i < photographer.length; i++)
+  {
+    if (photographer[i].id == photographId)
+    {
+      photographCourant=photographer[i]
+    }
+  }
+
+  console.log(photographCourant);
+  console.log(portfolio)
+
+});
 
 // Récupérer tous les liens 
 const links = document.querySelectorAll('a');
@@ -36,32 +94,7 @@ link.addEventListener('click', event => {
 
 });
 
-
-const url = window.location.href;
-console.log('url de la page est', url);
-let arr = url.split("=")[1];
-
-
-fetch('data/photographers.json')
-.then(response => response.json())
-.then(data => {
-  const photographers = data['photographers'];
-
-  for (let i = 0; i < photographers.length; i++) {
-    const photographerData = photographers[i];
-   
- 
-
-    // Créer un élément <p> pour afficher le nom du photographe
-    const nameElement = document.createElement('p');
-    nameElement.innerHTML = photographerData.name;
-
-    // Ajouter l'élément au conteneur approprié dans votre page HTML
-    const container = document.getElementById('photographersContainer');
-    container.appendChild(nameElement);
-  }
-});
-
+//-------------------------------------------------------------------------
 
 
 
